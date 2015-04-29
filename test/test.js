d4f2e70f4ff4cf2761addf700eb991d5d987095e
Jose Diaz <jdiaz31@Joses-iMac.fios-router.home>
2015-04-29 17:22:51 -0400

    add more tests

--- test/test.js
+++ test/test.js
@@ -18,3 +18,15 @@ describe('Cash Register', function(){
 		})
 	})
 });
+
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
+
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+})
+
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+});
