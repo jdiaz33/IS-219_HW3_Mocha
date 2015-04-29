1a7761e82aad6dd7c6884991fbc7377af43b5930
Jose Diaz <jdiaz31@Joses-iMac.fios-router.home>
2015-04-29 16:57:26 -0400

    first test

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require("assert"); // node.js core module
+
+describe('Array', function(){
+	describe('#indexOf()', function(){
+		it('should return -1 when the value is not present', function(){
+			assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
+		})
+	})
+});
+
+var C = require('../cash.js'); // our module
+
+describe('Cash Register', function(){
+	describe('Module C', function(){
+		it('should have a getChange Method', function(){
+			assert.equal(typeof C, 'object');
+			assert.equal(typeof C.getChange, 'function');
+		})
+	})
+});
